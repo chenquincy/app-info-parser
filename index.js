4d26a9af44040b687a750d7ee3bb1a4fc8ee2aeb
chenquincy <mail@quincychen.cn>
2018-11-26 21:13:42 +0800

    webpack build

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/app-info')
+module.exports = require('./dist/index')
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  node: {
+    fs: 'empty'
+  }
+}
