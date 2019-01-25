4832e1321ae8fd4f1826d2fc7b00c6f6bfeb729d
chenquincy <mail@quincychen.cn>
2019-01-25 11:56:16 +0800

    release: 0.0.8 fix: absolute path error

--- example/node.js
+++ example/node.js
@@ -1,5 +1,5 @@
 const AppInfoParser = require('../src/index.js')
-const parser = new AppInfoParser('../packages/test.ipa')
+const parser = new AppInfoParser('./../packages/test.ipa')
 parser.parse().then(result => {
   console.log(' ----> ', result)
 }).catch(e => {
